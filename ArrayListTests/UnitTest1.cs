647fefa3652759641bbd3d11ab905a240dca11cc
Franskevich <oorlykino@gmail.com>
2021-03-19 18:50:46 +0300

    new methods and tests

--- ArrayListTests/UnitTest1.cs
+++ ArrayListTests/UnitTest1.cs
@@ -7,14 +7,43 @@ namespace ArrayListTests
     public class Tests
     {
 
-        [TestCase(5, new int[] { 1, 4 })]
-        public void AddFirst(int value, int[] expectedArray)
+        //[TestCase(5, new int[] { 1, 4 })]
+        //public void AddFirst(int value, int[] expectedArray)
+        //{
+        //    ArrayList expected = new ArrayList(expectedArray);
+        //    ArrayList actual = new ArrayList();
+        //    actual.AddFirst(value);
+
+
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+        [Test]
+        public void AddFirst()
+        {
+            ArrayList expected = new ArrayList();
+            expected.Add(2);
+            expected.Add(6);
+            expected.Add(77);
+            expected.Add(3);
+            expected.Add(6);
+            expected.Add(3);
+            expected.Add(3);
+            expected.Add(-5);
+            expected.Add(3);
+            expected.Add(3);
+            expected.Add(2);
+
+            expected.RemoveByIndexNElements(3,5);
+
+        }
+
+        [TestCase(3, new int[] { 2, 6, 77 }, new int[] { 2, 6, 77, 3 })]
+        public void RemoveByIndexTests(int index, int[] expectedArray, int[] actualArray)
         {
             ArrayList expected = new ArrayList(expectedArray);
-            ArrayList actual = new ArrayList();
-            actual.AddFirst(value);
-
-
+            ArrayList actual = new ArrayList(actualArray);
+            actual.RemoveByIndex(index);
             Assert.AreEqual(expected, actual);
         }
 
@@ -23,5 +52,15 @@ namespace ArrayListTests
         //{
         //    Assert.AreEqual(excepted, actual)
         //}
+
+
+        [TestCase(3, 5, new int[] { 2, 6, 77, 3, 3, 2 }, new int[] { 2, 6, 77, 3, 6, 3, 3, -5, 3, 3, 2 })]
+        public void RemoveByIndexNElementsTests(int index, int n, int[] expectedArray, int[] actualArray)
+        {
+            ArrayList expected = new ArrayList(expectedArray);
+            ArrayList actual = new ArrayList(actualArray);
+            actual.RemoveByIndexNElements(index, n);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
--- List/ArrayList.cs
+++ List/ArrayList.cs
@@ -9,7 +9,7 @@ namespace List
 
         public int L { get; private set; } // длина свойство
         private int[] _array;                  //фактический массив,  не видимый пользователю
-        // индексатор
+// индексатор
         public int this[int index]
         {
             get
@@ -39,7 +39,7 @@ namespace List
             _array = new int[10];
         }
 
-        //1 добавление значения в конец
+//1 добавление значения в конец
         public void Add(int value)
         {
             if (L == _array.Length)
@@ -67,14 +67,14 @@ namespace List
         {
             int newlength = (int)(_array.Length * 0.67d + 1);
             int[] tmpArray = new int[newlength];
-            for (int i = 0; i < _array.Length; i++)
+            for (int i = 0; i < newlength; i++)
             {
                 tmpArray[i] = _array[i];
             }
             _array = tmpArray;
         }
 
-        //2 добавить значение в начало
+//2 добавить значение в начало
         public void AddFirst(int value)
         {
             if (L == _array.Length)
@@ -91,7 +91,7 @@ namespace List
             L++;
         }
 
-        //3 добавление значения по индексу
+//3 добавление значения по индексу
         public void AddByIndex(int value, int index) //narushen single responsobility сделать метод мув с
         {
             if (L == _array.Length)
@@ -108,14 +108,14 @@ namespace List
             L++;
         }
 
-        //4 удаление из конца 1 элемента
+//4 удаление из конца 1 элемента
         public void RemoveLast()
         {// ogranichit dostup polzovately srabotaet))
             int n = 1;
-            RemoveLastElements(n)
+            RemoveLastElements(n);
         }
 
-        //5 удаление из начала одного элемента
+//5 удаление из начала одного элемента
         public void RemoveFirst()// перенести в массиве 123 всё на 1 шаг вперед будет быстрее. Так же сделать в ремоут(делит) индекс
         {
             int n = 1;
@@ -126,19 +126,22 @@ namespace List
                 DownSize(); //zavisit ot novoi Length! umenshaem dlinu do nego
             }
         }
-        //6 удаление по индексу одного элемента
+//6 удаление по индексу одного элемента
         public void RemoveByIndex(int index)// не создвать лишний массив а присваивать в этом массиве сдедующие значения
         {// отбить ошибки -900
+            
             int n = 1;
-            RemoveIndexN(index, n);
 
-            L -= 1;
-            if (L <= (_array.Length / 2))
+            RemoveByIndexNElements(index, n);
+
+            // L -= 1;
+
+            if (L < (_array.Length / 2))
             {
                 DownSize();
             }
         }
-        //7 удаление из конца N элементов
+//7 удаление из конца N элементов
         public void RemoveLastElements(int n)
         {          // ogranichit dostup polzovately srabotaet))
             if (n <= 0)
@@ -152,7 +155,7 @@ namespace List
             }
         }
 
-        //8 удаление из начала N элементов 
+//8 удаление из начала N элементов 
         public void RemoveFirstN(int n)
         {
             int[] tmpArray = new int[_array.Length];
@@ -168,56 +171,63 @@ namespace List
                 DownSize(); //zavisit ot novoi Length! umenshaem dlinu do nego
             }
         }
-        //9 удаление по индексу N элементов
-        public void RemoveIndexN(int index, int n)
+//9 удаление по индексу N элементов
+        public void RemoveByIndexNElements(int index, int n)
         {
-            int[] tmpArray = new int[_array.Length];
-            for (int i = 0; i < index - 1; i++)
+            //int[] tmpArray = new int[_array.Length];
+            //for (int i = index; i < L-n; i++)
+            //{               
+            //    tmpArray[i] = _array[i+n];
+            //}
+
+            //for (int i=0; i<index; i++)
+            //{
+            //    tmpArray[i] = _array[i];
+            //}
+            //_array = tmpArray;
+
+            for (int i = index; i < L-n; i++)
             {
-                tmpArray[i] = _array[index + n];
+                _array[i] = _array[i + n];
             }
-            _array = tmpArray;
+
             L -= n;
-            if (L <= (_array.Length / 2))
+            if (L < (_array.Length / 2))
             {
                 DownSize();
             }
         }
-        //10 вернуть длину
+//10 вернуть длину
         public int GetLength()
         {
             return L;
         }
 
-        public int GetIndex(int index)
-        {
-            return _array[index]; // pravilo izbutochnosti
-        }
-        //11 доступ по индексу
+//11 доступ по индексу
         public int GetElementByIndex(int index)
         {
-            return _array[index];
+            return _array[index]; // правило избыточности
         }
-        //12 первый индекс по значению
-        public int GetIndexByValue(int x)
+//12 первый индекс по значению
+        public int GetFirstIndexByValue(int x)
         {
-            int index = -1;
-            for (int i = 0; i < L; i++)// return _array[index]; ...
+            int index = 0;
+            for (int i = 0; i < L-1; i++)   // return _array[index]; ...
             {
-                if (_array[i] ==x)
+                if (_array[i] == x)
                 {
-                    index = i + 1;
+                    index = i;
                     break;
                 }               
             }
             return index;
         }
-        //13 изменение по индексу
-        public void ChangeValueByIndexN(int index, int value)
+//13 изменение по индексу
+        public void ChangeValueByIndex(int index, int value)
         {
-            _array[index - 1] = value;
+            _array[index] = value;
         }
-        //14 реверс
+//14 реверс
         public void Reverse()
         {
             int tmpArrayX;
@@ -229,11 +239,11 @@ namespace List
                 _array[x] = tmpArrayX;
             }
         }
-        //15 поиск значения максимального элемента
+//15 поиск значения максимального элемента
         public int GetMax()
         {
             int max = _array[0];
-            for (int i =1; i<L; i++)
+            for (int i = 1; i<L; i++)
             {
                 if (_array[i]>max)
                 {
@@ -243,16 +253,49 @@ namespace List
             return max;
         }
 
-        //16    поиск значения минимального элемента   
-        public int GetMin()
+//16    поиск значения минимального элемента   
+        public int GetValueOfMinElement(int n)
+        {
+            int min=_array[0];
+            for(int i=0; i<L; i++)
+            {
+                if(_array[i]<min)
+                {
+                    min = _array[i];
+                }
+            }
+            return min;
+        }
 
-        //17 поиск индекс максмального элемента
-        public int GetMaxIndex()
+//17 поиск индекс максмального элемента
+        public int GetIndexOfMaxValue(int n)
+        {
+            int max = 0;
+            for (int i = 0; i < L; i++)
+            {
+                if (_array[i] > _array[max])
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
 
-        //18 поиск индекс минимального элемента
-        public int GetMinIndex()
+//18 поиск индекс минимального элемента
+        public int GetIndexOfMinValue()
+        {
+            int min = 0;
+            for (int i = 0; i < L; i++)
+            {
+                if (_array[i] <  _array[min])
+                {
+                    min = i;
+                }
+            }
+            return min;
+        }
 
-        //19 Сортировка по возрастанию
+//19 Сортировка по возрастанию
         public void SortByIncrease()
         {
             int tmp;
@@ -262,30 +305,110 @@ namespace List
                 {
                     if (_array[i] > _array[j])
                     {
-                        tmp = array[i];
+                        tmp = _array[i];
                         _array[i] = _array[j];
                         _array[j] = tmp;
                     }
-                }
-                
-            
+                }                      
             }
         }
 
-        //20 Сортировка по убыванию
+//20 Сортировка по убыванию
         public void SortByDecrease()
+        {
+            int tmp;
+            for (int i=0; i<L; i ++)
+            {
+                for (int j = i + 1; j<L; j++)
+                {
+                    if (_array[i] < _array[j])
+                    {
+                        tmp = _array[i];
+                        _array[i] = _array[j];
+                        _array[j] = tmp;
+                    }
+                }                      
+            }
+        }
 
-        //21 удаление по значению первого(?вернуть индекс)
-        public int RemoveFirstByValue (int x)
+//21 удаление по значению первого (?вернуть индекс)
+        //public int RemoveFirstByValue (int x)
+        public void RemoveByValueFirst(int value)// не создвать лишний массив а присваивать в этом массиве сдедующие значения
+        {
+            for (int i = 0; i < L; i++)
+            {
+                if (_array[i] == value)
+                {
+                    RemoveByIndex(i);
+                    return;
+                }
+            }
+        }
+            
 
-        //22 Удаление по значению всех(?вернуть кол-во)
-        public int RemoveAllX(int x)
+//22 Удаление по значению всех(?вернуть кол-во)
+        public void RemoveByValueAll(int value)
+        {
+            for (int i = 0; i < L; i++)
+            {
+                if (_array[i] == value)
+                {
+                    RemoveByIndex(i);                   
+                }
+            }
+        }
 
-            //23.1 Конструктор по умолчанию
-            //23.2 Конструктор на основе 1 элемента
-            //23.3 конструктор на основе массива
-            //24 дообавлеение нашего списка в конец
-            //25 добавление списка в начало
-            //26 добавление списка по индексу
+
+        //23.1 Конструктор по умолчанию
+
+        //23.2 Конструктор на основе 1 элемента
+
+        //23.3 конструктор на основе массива
+        public ArrayList(int[]array)
+        {                        
+            L = array.Length;
+            _array = array;
+            UpSize();
+        }
+
+
+        //24 дообавлеение нашего списка в конец
+
+        //25 добавление списка в начало
+
+        //26 добавление списка по индексу
+        AddArrayListAtIndex
+
+
+
+        public override string ToString()
+        {
+            string s = "";
+            for(int i = 0; i < L; i++)
+            {
+                s += _array[i] + " ";
+            }
+            return s;
+        }
+        public override bool Equals(object obj)//kак он будет сравнивать элементы что бы асерт работал
+        {
+            ArrayList arrayList = (ArrayList)obj;
+            if (this.L != arrayList.L)
+            {
+                return false;
+            }
+            for(int i=0; i<L; i++)
+            {
+                if (this._array[i] != arrayList._array[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
